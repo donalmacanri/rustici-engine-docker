cb5cc8d5f1d2bd4df239bdcb8256f8c7e7013791
Donal Mac An Ri <donal@bluedrop.com>
2024-10-21 15:33:58 -0230

    feat: Update server.py to use relative path for templates

--- my-lms/server.py
+++ my-lms/server.py
@@ -3,23 +3,25 @@ from aiohttp import web
 import jinja2
 import aiohttp_jinja2
 
+
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    context = {'name': name}
-    response = aiohttp_jinja2.render_template('index.html', request, context)
+    name = request.match_info.get("name", "Anonymous")
+    context = {"name": name}
+    response = aiohttp_jinja2.render_template("index.html", request, context)
     return response
+
 
 async def main():
     app = web.Application()
-    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('my-lms/templates'))
-    app.add_routes([web.get('/', handle),
-                    web.get('/{name}', handle)])
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
+    app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, '0.0.0.0', 8080)
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
     await site.start()
     print(f"Serving on http://localhost:8080")
     await asyncio.Event().wait()  # run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
