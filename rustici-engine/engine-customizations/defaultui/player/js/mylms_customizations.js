1c3132e222d351b07af1fe1668e954617a370c8d
Donal Mac An Ri <donal@bluedrop.com>
2024-10-24 10:36:29 -0230

    fix: update link to view registrations

--- rustici-engine/engine-customizations/defaultui/player/js/mylms_customizations.js
+++
@@ -1,6 +0,0 @@
-if (top != self) {
-    $("body").addClass("hideNavBar");
-    window.rscpCfg.set('topParams', {...window.rscpCfg.get('topParams'), showNavBar: false});
-    calcContentFrameSize();
-}
-window.rscpCustomizationCompleted();
